b6e1e76e36fac3708cee330cab680681ec66d0b9
dracu-lah <nevilnicks4321@gmail.com>
2023-03-01 14:58:22 +0530

    added loader with  darkmode

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import MainLayout from "../Layouts/MainLayout";
-export default function Results() {
+import MainLayout from "./Layouts/MainLayout";
+
+function App() {
   return (
     <MainLayout>
       <div className="flex justify-center text-slate-100 text-4xl py-[50vh]">
-        Results
+        Home
       </div>
     </MainLayout>
   );
 }
+
+export default App;
